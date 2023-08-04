beb3714092edc7124b93a12e5f09fff587314290
Sujin Jeon <mackerel0106@gmail.com>
2023-08-04 13:54:28 +0900

    Fix: mysql 연결
    async 함수를 통해 블로킹을 하니 문제없이 DB 연결이 되는것을 확인
    이전까지는 처음 컨테이너를 실행했을 경우 ECONNREFUSED가 무조건 발생

--- src/db/index.js
+++ src/db/index.js
@@ -1,19 +1,40 @@
 import mysql from 'mysql2';
+import { createClient } from 'redis';
 
-const db = mysql.createConnection({
+const options = {
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER_NAME,
   password: process.env.MYSQL_USER_PASSWORD,
   database: process.env.MYSQL_DATABASE,
+  port: 3306,
+};
+
+let db;
+(async () => {
+  db = mysql.createPool(options);
+
+  db.on('connect', () => {
+    console.log('MySQL connected\n');
+  });
+  db.on('error', (error) => {
+    console.error('MySQL failed...\n', error);
+  });
+})();
+
+// Redis
+const redisClient = createClient({
+  url: process.env.REDIS_URL,
 });
 
-db.connect();
+(async () => {
+  redisClient.on('connect', () => {
+    console.log('Redis connected!');
+  });
+  redisClient.on('error', (error) => {
+    console.error('Redis Client Error', error);
+  });
 
-db.on('connect', () => {
-  console.log('MySQL connected!');
-});
-db.on('error', (error) => {
-  console.error('MySQL failed...\n', error);
-});
+  await redisClient.connect();
+})();
 
 export default db;
