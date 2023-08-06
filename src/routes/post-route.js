46da38a8db8e10b886f89814150c77ccebaa9f8f
Sujin Jeon <mackerel0106@gmail.com>
2023-08-06 17:27:49 +0900

    Feat: api 엔드포인트 작성

--- src/routes/post-route.js
+++ src/routes/post-route.js
@@ -3,10 +3,22 @@ import { postsService, tokenHandler } from '../services';
 
 const postsRouter = Router();
 
+// 새 게시글 생성
 postsRouter.post(
   '/api/v1/posts',
   tokenHandler.verifyAccessToken,
   postsService.insertPost
 );
 
+// pagination으로 게시글 조회
+postsRouter.get('/api/v1/posts');
+
+// 특정 게시글 조회
+postsRouter.get('/api/v1/posts/:id');
+
+// 특정 게시글 수정
+postsRouter.put('/api/v1/posts/:id');
+
+// 특정 게시글 삭제
+postsRouter.delete('/api/v1/posts/:id');
 export default postsRouter;
